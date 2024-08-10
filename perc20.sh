c777b609482a195ea7e95bf54ce2ee832197d628
RPCdot.com <141464235+RPCdotcom@users.noreply.github.com>
2024-08-10 18:29:37 +0300

    Update perc20.sh

--- perc20.sh
+++ perc20.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
-curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
+curl -s https://raw.githubusercontent.com/RPCdotcom/Swisstronik/main/logo.sh | bash
 sleep 4
 
 sudo apt-get update && sudo apt-get upgrade -y
